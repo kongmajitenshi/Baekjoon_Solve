3c66b53d2df1a4bcb703a6ce107f9ee011b2821d
kongmajitenshi <dksk3504@gmail.com>
2024-08-26 12:17:45 +0900

    240826

---
+++ src/Answer/baekjoon_10171.java
@@ -0,0 +1,10 @@
+package Answer;
+
+public class baekjoon_10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
---
+++ src/Answer/baekjoon_10998.java
@@ -0,0 +1,17 @@
+package Answer;
+
+import java.util.Scanner;
+
+public class baekjoon_10998 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a*b);
+
+        
+        sc.close();
+    }
+}
