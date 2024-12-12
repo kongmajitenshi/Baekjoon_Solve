2ee94ba6cb0746d82d5cbbca554f53a430d2d45e
kongmajitenshi <dksk3504@gmail.com>
2024-12-12 10:42:54 +0900

    241212

--- src/baekjoon_1427.java
+++ src/baekjoon_1427.java
@@ -1,5 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class baekjoon_1427 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        int[] nums = new int[input.length()];
+        for(int i=0; i<input.length(); i++){
+            nums[i] = Character.getNumericValue(input.charAt(i));
+        }
+        Arrays.sort(nums);
+        for(int i=nums.length-1; i>=0; i--){
+            System.out.printf("%d", nums[i]);
+        }
     }
 }
