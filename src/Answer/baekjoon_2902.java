051505c1100c795b0288e29c1c1b88b951b7ba77
kongmajitemshi <dksk3504@gmail.com>
2025-05-22 10:44:35 +0900

    250522_2902java

---
+++ src/Answer/baekjoon_2902.java
@@ -0,0 +1,19 @@
+package Answer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class baekjoon_2902 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(st.nextToken(), "-");
+
+        while((st.hasMoreTokens())){
+            System.out.print(st.nextToken().charAt(0));
+        }
+        br.close();
+    }// main
+}// class
