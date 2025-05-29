f5dcad1e065c1429af7dd0623bcdb1949a812901
kongmajitemshi <dksk3504@gmail.com>
2025-05-29 10:30:11 +0900

    250523_2093java

--- src/Answer/baekjoon_2903.java
+++ src/Answer/baekjoon_2903.java
@@ -1,5 +1,24 @@
 package Answer;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class baekjoon_2903 {
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int dot = 2;
+        int result = 0;
+
+        for(int i=0; i<n; i++){
+            dot = dot*2-1;
+            result = dot*dot;
+        }
+        System.out.println(result);
+
+        br.close();
+    }
 }
