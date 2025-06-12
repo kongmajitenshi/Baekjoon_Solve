45046a2ca06dc8a582058e84ae83ca7eec21134b
kongmajitemshi <dksk3504@gmail.com>
2025-06-12 10:50:47 +0900

    250612-2750-solved-java

---
+++ src/Answer/baekjoon_2750.java
@@ -0,0 +1,21 @@
+package Answer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class baekjoon_2750 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] nums = new int[n];
+        for(int i=0; i<n; i++){
+            nums[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(nums);
+        for(int num : nums){
+            System.out.println(num);
+        }
+    }
+}
