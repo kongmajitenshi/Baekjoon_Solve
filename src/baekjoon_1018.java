cad25ad55317e8ca5ebff098ddc35aed0dd798c2
kongmajitemshi <dksk3504@gmail.com>
2025-07-24 17:09:04 +0900

    250724-solved-1018l

--- src/baekjoon_1018.java
+++ src/baekjoon_1018.java
@@ -8,22 +8,31 @@ public class baekjoon_1018 {
         String input = br.readLine();
         int n = Integer.parseInt(input.split(" ")[0]),  // 세로 
         m = Integer.parseInt(input.split(" ")[1]);  // 가로
+
         String wStart = "WBWBWBWB", bStart = "BWBWBWBW";
-        int count = 0;
+        // int count = 0;  // 몇 개 바꿔야하나 세서 저장. 결과적으로 이 변수가 출력될 것.
         char[][] chess = new char[n][m];
         for(int i=0; i<n; i++){
             String in = br.readLine();
             for(int j=0; j<m; j++){
-                chess[i][j] = in.charAt(j);
+                chess[i][j] = in.charAt(j);  // 입력받은 값 저장
             }
         }
-        String result;
-        for(int i=0; i<m; i++){
-            
-            System.out.print(chess[0][i]);
-        }
-        
 
+        int minCount = Integer.MAX_VALUE;
+        for(int i=0; i<= n-8; i++){
+            for(int j=0; j<= m-8; j++){
+                int wCount = 0, bCount = 0;  // 몇개 바뀌었는지 체크. wCount는 화이트가 바뀐거.
+                for(int k=0; k<8; k++){
+                    for(int l=0; l<8; l++){  // 여기서부터 본격적으로 체크. 
+                        if(chess[i+k][j+l] != ((k%2==0) ? wStart.charAt(l):bStart.charAt(l))) wCount++;
+                        if(chess[i+k][j+l] != ((k%2==0) ? bStart.charAt(l):wStart.charAt(l))) bCount++;
+                    }
+                }
+                minCount = Math.min(minCount, Math.min(wCount, bCount));
+            }
+        }
+        System.out.println(minCount);
 
 
     }  // main
