e22e20d5c493d34b4d3dd87489595bd36b3e73d2
kongmajitemshi <dksk3504@gmail.com>
2025-09-02 16:12:13 +0900

    250902_solved_1920

--- src/Answer/baekjoon_1920.java
+++ src/Answer/baekjoon_1920.java
@@ -3,38 +3,33 @@ package Answer;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
 
 public class baekjoon_1920 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int originSize = Integer.parseInt(br.readLine());  // n
-        int[] originNums = new int[originSize];  
-        String input = br.readLine();
-        for(int i=0; i<originSize; i++){
-            originNums[i] = Integer.parseInt(input.split(" ")[i]);  // n개의 정수
-        }
-        int checkSize = Integer.parseInt(br.readLine());  // m
-        int[] checkNums = new int[checkSize];
-        input = br.readLine();
-        for(int i=0; i<checkSize; i++){
-            checkNums[i] = Integer.parseInt(input.split(" ")[i]);  // m개의 정수
+
+        int n = Integer.parseInt(br.readLine());
+        Set<Integer> originSet = new HashSet<>();
+        String[] nNums = br.readLine().split(" ");
+        for(String s : nNums){
+            originSet.add(Integer.parseInt(s));
         }
 
-        for(int i=0; i<originSize; i++){
-            boolean check = false;
-            for(int j=0; j<checkSize; j++){
-                if(originNums[j] == checkNums[i]){
-                    check = true;
-                    break;
-                }
-            }
-            if(check == true){
-                System.out.println(1);
+        int m = Integer.parseInt(br.readLine());
+        String[] mNums = br.readLine().split(" ");
+        StringBuilder sb = new StringBuilder();
+
+        for(String s : mNums){
+            if(originSet.contains(Integer.parseInt(s))){
+                sb.append("1\n");
             }else{
-                System.out.println(0);
+                sb.append("0\n");
             }
         }
-        // n개의 정수 안에 m개의 정수가 있나 없나 판별하는거임. 출력 m은 m개의 정수 순서대로 체크한 결과 출력,
-        // 예시) n정수가 1 3 5 7 9 이고 m정수가 1 2 3 임. 1, 3은 있고 2는 없으니 출력할 땐 1 0 1 이 출력되는 것. 이런 느낌임.
+
+        System.out.println(sb);
+
     }
 }
