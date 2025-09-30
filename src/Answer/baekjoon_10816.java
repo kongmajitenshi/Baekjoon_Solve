6428376c9cdbd5fa98d39c0be5e608c8e6463a8d
kongmajitemshi <dksk3504@gmail.com>
2025-09-30 09:28:11 +0900

    10816_solving

---
+++ src/Answer/baekjoon_10816.java
@@ -0,0 +1,13 @@
+package Answer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class baekjoon_10816 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        
+    }   
+}
